20f85d8c7d9ce9d01cb6d5a281fd77270fc0b556
Vladislav <vasilevich.account@yandex.ru>
2022-10-11 22:02:37 +0300

    Выполнено третье задание

---
+++ Task_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N > 0)
+{
+  for (int i = 1; i <= N; i++)
+  {
+    Console.WriteLine($"{i,3} | {i * i * i}");
+  }
+}
+else
+{
+  Console.WriteLine("Ошибка: введено число меньше 1");
+}
